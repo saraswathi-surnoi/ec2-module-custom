8297aa5418e2f2e895ac083fa8f9c173e505560c
YourUsername <saraswathinandula@surnoi.com>
2025-11-03 18:48:32 +0530

    made changes

--- customize-ec2-module/sg-module/main.tf
+++ customize-ec2-module/sg-module/main.tf
@@ -1,23 +1,33 @@
 resource "aws_security_group" "security_group" {
   name        = var.security_group.name
   description = var.security_group.description
-  vpc_id      = var.vpc_id    
+  vpc_id      = var.vpc_id
   tags        = var.security_group.tags
 }
 
+# Ingress rules
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
-  for_each          = var.security_group_ingress
+  for_each = {
+    for idx, rule in var.security_group_ingress : idx => rule
+  }
+
   security_group_id = aws_security_group.security_group.id
-  cidr_ipv4         = each.value.cidr_ipv4
-  ip_protocol       = each.value.ip_protocol
+  cidr_ipv4         = each.value.cidr_blocks[0]  # take the first CIDR (since it's a list)
+  ip_protocol       = each.value.protocol
   from_port         = try(each.value.from_port, null)
   to_port           = try(each.value.to_port, null)
 }
 
+# Egress rules
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  for_each          = var.security_group_egress
+  for_each = {
+    for idx, rule in var.security_group_egress : idx => rule
+  }
+
   security_group_id = aws_security_group.security_group.id
-  cidr_ipv4         = each.value.cidr_ipv4
-  ip_protocol       = each.value.ip_protocol
+  cidr_ipv4         = each.value.cidr_blocks[0]
+  ip_protocol       = each.value.protocol
+  from_port         = try(each.value.from_port, null)
+  to_port           = try(each.value.to_port, null)
 }
 
