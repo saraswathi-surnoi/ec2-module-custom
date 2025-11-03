d05a3cd5404562df2fd5acdb5b60bf26a6dde7eb
YourUsername <saraswathinandula@surnoi.com>
2025-11-03 20:24:26 +0530

    made changes

--- customize-ec2-module/ec2-module/main.tf
+++ customize-ec2-module/ec2-module/main.tf
@@ -1,7 +1,17 @@
+
 resource "aws_instance" "ec2-instances" {
-  ami                    = var.ec2.ami
-  instance_type          = var.ec2.instance_type
-  key_name               = var.ec2.key_name
-  vpc_security_group_ids = var.ec2.vpc_security_group_ids
-  tags = var.ec2.tags
+  ami                    = data.aws_ami.ubuntu.id    # ✅ using AMI from data.tf
+  instance_type          = var.instance_type
+  subnet_id              = element(data.aws_subnets.selected.ids, 0)
+  vpc_security_group_ids = [var.security_group_id]
+  key_name               = var.key_pair_name
+  user_data              = file(var.user_data)        # ✅ taking path from variable map
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.instance_name
+    }
+  )
 }
+
--- customize-ec2-module/ec2-module/variables.tf
+++ customize-ec2-module/ec2-module/variables.tf
@@ -1,9 +1,39 @@
-variable "ec2" {
-  type = object({
-    ami = string
-    instance_type = string
-    key_name  = string
-    vpc_security_group_ids = list(string)
-    tags = map(string)
-  })
+variable "ami_id" {
+  description = "AMI ID for EC2 instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for EC2"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Key pair name to use for EC2 instances"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID for subnet selection"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID for EC2 instance"
+  type        = string
+}
+
+variable "user_data" {
+  description = "Path to user data script"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to assign to EC2 instances"
+  type        = map(string)
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  type        = string
 }
